ea1481148d97071a1844e637d5170c23df8b66cb
ADizzyMoon <165205398+ADizzyMoon@users.noreply.github.com>
2024-03-29 20:01:36 -0400

    Create quotecalc.js

---
+++ quotecalc.js
@@ -0,0 +1,25 @@
+let trackCount
+let releaseLength
+let vinylNeed
+let ddpNeed
+let jobtype
+let dayNumber
+let deadlinem
+let versionCount
+const masteringMax
+const masteringMin
+const mixingMax
+const mixingMin
+const hourlyMax
+const hourlyMin
+const spaceCost
+const ddpDelivery
+/* 
+assigning values to variables via user input. will this be done with a console? might be fun. forms are bad. some functions will have to determine follow up questions
+*/
+
+//next is calclating the type of job and choosing the math the program needs to do
+
+//after deciding, math will be done that generates minimum and maximum costs
+
+//all the separate sets of costs will be added and presented at the end
